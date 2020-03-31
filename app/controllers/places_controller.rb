09336989b1ca3f962054ab5c4b6cedbc98e66f58
akshaybharwani <akshaybharwani@pm.me>
2020-03-31 16:29:44 +0530

    Make basic Place show View and Action with dynamic titles

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,15 @@
 class PlacesController < ApplicationController
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def index
+    @places = Place.all
+  end
+
 end
